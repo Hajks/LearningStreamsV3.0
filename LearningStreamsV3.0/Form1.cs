a53ee32737cd99534c273b2abc48a14ba26eaaca
Hajk <tunczorx@gmail.com>
2018-07-03 13:31:10 +0200

    Using switch

--- LearningStreamsV3.0/Form1.cs
+++ LearningStreamsV3.0/Form1.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LearningStreamsV3._0
 {
+
     public partial class Form1 : Form
     {
+
+        
+        List<Card> cards = new List<Card>();
+        enum BodyPart
+        {
+            Head,
+            Shoulders,
+            Knees,
+            Toes
+        }
         public Form1()
         {
             InitializeComponent();
+            Suits suit = new Suits();
+            suit.Hello.Split();
+            MessageBox.Show(suit.Hello);
+           
         }
+        private void WritePartInfo(BodyPart part, StreamWriter writer)
+        {
+            if (part == BodyPart.Head)
+                writer.WriteLine("głowa jest owłosiona");
+            else if (part == BodyPart.Shoulders)
+                writer.WriteLine("ramiona są szerokie");
+            else if (part == BodyPart.Knees)
+                writer.WriteLine("kolana są guzowate");
+            else if (part == BodyPart.Toes)
+                writer.WriteLine("palce są malutkie");
+            else
+                writer.WriteLine("nieokreślona część jest nieokreślona");
+        }
+        private void WritePartInfoBySwitch(BodyPart part, StreamWriter writer) // Switch is easier to write and makes code more readable.
+        {
+            switch (part)
+            {
+                case BodyPart.Head:
+                    writer.WriteLine("głowa jest owłosiona");
+                    break;
+                case BodyPart.Shoulders:
+                    writer.WriteLine("ramiona są szerokie");
+                    break;
+                case BodyPart.Knees:
+                    writer.WriteLine("kolana są guzowate");
+                    break;
+                case BodyPart.Toes:
+                    writer.WriteLine("palce są malutkie");
+                    break;
+                default:
+                    writer.WriteLine("nieokreślona część jest nieokreślona");
+                    break;
+            }
+        }
+        public void WriteCard(string FileName) //writing cards name to .txt
+        {
+            using (StreamWriter writer = new StreamWriter(FileName))
+            {
+                for (int i = 0; i < cards.Count; i++)
+                {
+                    //writer.WriteLine(cards[i].Name);
+                }
+            }
+        }
+
+        
+        
     }
 }
